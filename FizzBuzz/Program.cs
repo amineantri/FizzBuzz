05efb2754f1e241bc6acee20eb9099d729a7f98c
imoum <amine.antri@gmail.com>
2021-08-28 12:29:22 +0200

    first commit

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzzer fb = new FizzBuzzer();
+            for(int i = 0; i < 101; i++)
+            {
+                Console.WriteLine(fb.Transform(i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
